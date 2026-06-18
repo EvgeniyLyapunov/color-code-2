2c049f56bc33dcc24bb91a44dd6346a4b3ae3747
EvgeniyLyapunov <lyapunovcs@gmail.com>
2026-06-18 06:31:01 +0300

    настроил vite, автоимпорты и типизацию окружения

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/auto-imports.d.ts',
+      vueTemplate: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  // Настройки сервера для разработки
+  server: {
+    host: '127.0.0.1',
+    port: 1008,
+    open: true,
+  },
+});
