2452baaab8d230b18e9639416f1e6b6ec36a5436
EvgeniyLyapunov <lyapunovcs@gmail.com>
2026-07-04 19:48:30 +0300

    модальное окно GameModal

--- src/modals/home-page/game-modal/GameModal.vue
+++ src/modals/home-page/game-modal/GameModal.vue
@@ -5,7 +5,7 @@
     persistent
     transition-show="scale"
     transition-hide="scale">
-    <q-card class="modal">
+    <q-card class="modal" flat :bordered="false">
       <!-- header -->
       <q-card-section class="modal-header">
         <div class="modal-header__title">
@@ -15,6 +15,32 @@
         <q-space />
         <q-btn class="modal-header__icon-close" icon="sym_o_close" flat round dense v-close-popup />
       </q-card-section>
+      <q-list class="modal-menu" style="width: 100%">
+        <q-item class="modal-menu__item" clickable v-ripple>
+          <q-item-section avatar style="min-width: 30px">
+            <q-icon class="modal-menu__item-icon" name="sym_o_flag_2" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="modal-menu__item-label">Начать новую игру</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="modal-menu__item" clickable v-ripple>
+          <q-item-section avatar style="min-width: 30px">
+            <q-icon class="modal-menu__item-icon" name="sym_o_bookmark_flag" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="modal-menu__item-label">Продолжить сохранённую игру</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item class="modal-menu__item" clickable v-ripple>
+          <q-item-section avatar style="min-width: 30px">
+            <q-icon class="modal-menu__item-icon" name="sym_o_bookmark_remove" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="modal-menu__item-label">Удалить сохранённую игру</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </q-card>
   </q-dialog>
 </template>
